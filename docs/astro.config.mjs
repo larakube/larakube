fb447eae34921d1d736c5cb3a34adf1b1158a4e9
korridor <26689068+korridor@users.noreply.github.com>
2025-06-11 15:33:12 +0200

    Add sitemap to docs

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import starlight from '@astrojs/starlight';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://www.larakube.com',
 	integrations: [
 		starlight({
 			title: 'LaraKube',
@@ -38,5 +40,6 @@ export default defineConfig({
 				},
 			],
 		}),
+		sitemap(),
 	],
 });
